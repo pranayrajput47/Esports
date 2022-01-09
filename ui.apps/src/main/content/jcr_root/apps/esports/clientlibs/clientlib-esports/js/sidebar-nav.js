8d74acddbbac2f00bb2a4e7c6ef14b60171cf7f5
Pranay Rajput <pranay.rajput@techchefz.com>
2022-01-10 03:47:38 +0530

    Clientlibs and resources setup

---
+++ ui.apps/src/main/content/jcr_root/apps/esports/clientlibs/clientlib-esports/js/sidebar-nav.js
@@ -0,0 +1,17 @@
+//toggle sidebar script
+
+let btn = document.querySelector("#toggle-menu-btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".search-btn");
+
+btn.onclick = function () {
+    sidebar.classList.toggle("active");
+    if (btn.classList.contains("bg-less")) {
+        btn.classList.replace("bg-less", "bg-expand");
+    } else {
+        btn.classList.replace("bg-expand", "bg-less");
+    }
+}
+searchBtn.onclick = function () {
+    sidebar.classList.toggle("active");
+}
