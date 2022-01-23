65963b62719e45c7802562f905516ffad011be74
Pranay Rajput <pranay.rajput@techchefz.com>
2022-01-24 05:00:14 +0530

    Tournament Component Changes

---
+++ ui.apps/src/main/content/jcr_root/apps/esports/components/tournamentTable/clientlibs-tournament/js/custom.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    fetchData("all");
+});
+
+$("#categories").on("change", function () {
+    var category = $('#categories :selected').val();
+    fetchData(category);
+});
+
+function fetchData(category) {
+    let content = [];
+    let contentPath = $('#tableContents').attr('root-path');
+    if (contentPath) {
+        $.ajax({
+            url: contentPath,
+            method: "GET",
+            contentType: 'application/json',
+            success: function (res) {
+                content = Object.values(res);
+                populate(content, category);
+
+            },
+            error: function (error) {
+
+            },
+        });
+    }
+}
+
+function populate(content, category) {
+    let element = [];
+    if (category != "all") {
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].gameType == category) {
+                element.push(content[i]);
+            }
+        }
+        createHTML(element);
+
+    } else {
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].instanceType) {
+                element.push(content[i]);
+            }
+        }
+        createHTML(element);
+    }
+}
+
+function createHTML(images) {
+    var rawTemplate = $("#tableTemplate").html();
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+    var ourGeneratedHTML = compiledTemplate(images);
+    $("#tableContents").html(ourGeneratedHTML);
+}
