880d327004f1f4b51d65f2130d0bdba28717a677
jbBhardwaj <97293490+jbBhardwaj@users.noreply.github.com>
2022-01-31 01:16:08 +0530

    sling model exporter for tournamentContentList

---
+++ core/src/main/java/com/esports/aem/core/models/TournamentContentListModel.java
@@ -0,0 +1,46 @@
+package com.esports.aem.core.models;
+
+import com.adobe.cq.export.json.ExporterConstants;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+
+@Getter
+@Model(adaptables = SlingHttpServletRequest.class,
+        resourceType = "esports/components/datasource/tournamentContentList",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+        extensions = ExporterConstants.SLING_MODEL_EXTENSION,selector = "model")
+public class TournamentContentListModel {
+
+
+
+    @Self
+    private SlingHttpServletRequest request;
+
+    @ValueMapValue
+    private String instanceType;
+
+    @ValueMapValue
+    private String gameType;
+
+    @ValueMapValue
+    private String gameName;
+
+    @ValueMapValue
+    private String time;
+
+    @ValueMapValue
+    private String date;
+
+    @ValueMapValue
+    private String logoImage;
+
+
+}
